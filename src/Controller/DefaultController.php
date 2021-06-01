a63e0e7c600c402064d2be3a6c8fcdde6b8e55f4
Emmanuel Rodriguez <emmanuel.rodriguez313@gmail.com>
2021-06-01 21:33:39 +0200

    Quest 04 Done

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -17,7 +17,7 @@ class DefaultController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('index.html.twig', [
+        return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
     }
